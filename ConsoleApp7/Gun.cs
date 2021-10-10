25aadf35c0a8bc2cbb1114cdd6379b21a5882e6e
Nijatasgar99 <nijataas@code.edu.az>
2021-10-11 00:11:18 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Gun.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    class Gun
+    {
+        public string M4;
+        public string  mod;/* avtomaticski, tek-tek*/
+        public  int sursatcount; /*daraqdaki gulle sayi*/
+        public  int Fullsursat; /*personal gulle saylari*/
+        public  int limitsursat; /*daragin limiti*/
+
+        public Gun()
+        {
+
+        }
+        public Gun(string silahnovu)
+        {
+            if (silahnovu == "M4")
+            {                  
+                sursatcount = 30;
+                limitsursat = 30;
+                Fullsursat = 300;
+            }            
+        }
+        public void Yuklenme()
+        {
+            if (sursatcount < limitsursat) 
+            {
+                sursatcount += 1;
+                Fullsursat -= 1;
+            }
+            else
+            {
+                Console.WriteLine("dolu");
+            }
+        }
+        public void Reload()
+        {
+            if (sursatcount == 0)
+            {
+                sursatcount = limitsursat;
+                Fullsursat = limitsursat;
+                Console.WriteLine("yenilendi");
+            }
+            else
+            {
+                Console.WriteLine("xais edirik yenileyin daragi ");
+            }
+           
+        }
+        public void ates()
+        {
+            if (mod=="avtomaticski")
+            {
+                for (int i = sursatcount; i < limitsursat; i--)
+                {
+                    sursatcount--;
+                    Console.WriteLine(sursatcount);
+                }
+                
+            }
+
+        }
+        public void tek()
+        {
+            if (mod == "tek-tek")
+            {
+                if (sursatcount > 0)
+                {
+                    sursatcount -= 1;
+                }
+                else
+                {
+                    Console.WriteLine("darag deyis");
+                }
+            }
+        }
+
+    }
+  
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gun M4 = new Gun("M4");
+            M4.Reload();
+            M4.Yuklenme();
+            
+            M4.tek();
+            M4.ates();
+            Console.WriteLine(M4.sursatcount);
+            
+        }
+    }
+}
